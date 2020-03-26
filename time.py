cf720e223c07051e177d02db175f9cfff0c0b735
binhades <binhades@users.noreply.github.com>
2020-03-26 09:58:32 +0800

    add mjd2fits function

--- time.py
+++ time.py
@@ -24,3 +24,19 @@ def utc2lst(obs_utc,scale='utc',format='isot'):
     lst = t.sidereal_time('mean')
 
     return lst
+
+def mjd2fits(hdul):
+
+    obs_utc = hdul[1].data['DATE-OBS']
+    obs_mjd = utc2mjd(obs_utc)
+
+    if 'MJD' in hdul[1].columns.names:
+        hdul[1].data['MJD'] = mjd
+    else:
+        new_col = fits.Column(name='MJD',format='1D',array=calstat)
+        newhdu = fits.BinTableHDU.from_columns(hdul[1].columns + fits.ColDefs([new_col]))
+        hdul[1].data = newhdu.data
+
+    hdul.flush()
+    return 1
+        

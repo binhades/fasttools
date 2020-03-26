a50e31e7425304010576dd22130f63c3316f4426
binhades <binhades@users.noreply.github.com>
2020-03-26 09:57:55 +0800

    get dec from filename instead of path folder name

---
+++ drift/dec.py
@@ -0,0 +1,20 @@
+#! /home/bliu/anaconda3/bin/python
+# Filename: dec.py
+# Aim: to get dec from the drift data path
+# file_str = '/data/fast/drift/Dec+0500/20180919/Dec+0500_drifting-M01_W_0001.fits'
+
+def get_dec(file_str):
+
+    try:
+        decM = float(str.split(path,'/')[-1][6:8])/60.
+        decD = float(str.split(path,'/')[-1][3:6])
+    except ValueError:
+        print("Error: dec get from path")
+        return 0
+
+    if decD > 0:
+        dec = decD + decM
+    else:
+        dec = decD - decM
+
+    return dec

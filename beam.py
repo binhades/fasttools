fbdb868a3601a049799bd2428a93fa6fa4b8c0d2
binhades <binhades@users.noreply.github.com>
2022-02-20 15:34:15 +0800

    use hdr.set instead of hdr.append, put BEAMID before EXTVER

--- beam.py
+++ beam.py
@@ -14,10 +14,12 @@ def get_beam_from_filename(fname,type='raw'):
     return beam 
 
 def beam2fits(hdul,beam=1):
-    if (not ('BEAMID' in hdul[1].header)):
-        hdul[1].header.append(('BEAMID',beam))
-    else:
-        hdul[1].header['BEAMID'] = beam
+
+    hdul[1].header.set('BEAMID',beam,'Beam number of FAST 19-beams',before='EXTVER')
+#    if (not ('BEAMID' in hdul[1].header)):
+#        hdul[1].header.append(('BEAMID',beam))
+#    else:
+#        hdul[1].header['BEAMID'] = beam
     hdul.flush()
     return 1
 

9cb5ac80dc84c62f11b0c801c379cf8bd1605c29
binhades <binhades@users.noreply.github.com>
2022-02-22 00:04:10 +0800

    improve speed if update=False

--- time.py
+++ time.py
@@ -1,6 +1,6 @@
-
 # Filename: time.py
 # Aim: to convert utc to mjd or lst at location of the FAST site
+
 
 def fast_time(obs_utc,scale='utc',format='isot'):
     from astropy.time import Time
@@ -26,19 +26,18 @@ def utc2lst(obs_utc,scale='utc',format='isot'):
 
 def mjd2fits(hdul,update=True):
 
-    from astropy.io import fits
-
     obs_utc = hdul[1].data['DATE-OBS']
     obs_mjd = utc2mjd(obs_utc) + 0.5*hdul[1].data['EXPOSURE'][0]/(24.0*3600.0)
 
-    if 'MJD' in hdul[1].columns.names:
-        hdul[1].data['MJD'] = obs_mjd
-    else:
-        new_col = fits.Column(name='MJD',format='1D',array=obs_mjd)
-        newhdu = fits.BinTableHDU.from_columns(hdul[1].columns + fits.ColDefs([new_col]))
-        hdul[1].data = newhdu.data
-
     if update:
+        if 'MJD' in hdul[1].columns.names:
+            hdul[1].data['MJD'] = obs_mjd
+        else:
+            from astropy.io import fits
+            new_col = fits.Column(name='MJD',format='1D',array=obs_mjd)
+            newhdu = fits.BinTableHDU.from_columns(hdul[1].columns + fits.ColDefs([new_col]))
+            hdul[1].data = newhdu.data
+
         hdul.flush()
     return obs_mjd
         

ce592155dd12aec04f0a272442ac74e8395f87a2
binhades <binhades@users.noreply.github.com>
2022-09-14 18:52:11 +0800

    add new func from hifast, to check observed_coor. it has its only problems. only for coor checking, not writing to fits hdr.

--- coor/__init__.py
+++ coor/__init__.py
@@ -1,5 +1,6 @@
 
 from .coor_table import coor_table
+from .coor_table import coor_table_multibeam
 from .coor_table import field_rotation
 from .coor2fits import radec2fits
 from .coor2fits import azel2fits
